b145ec7fd2276fdc381e28094ae6b8af2ca1cd66
Philip <philip.stuessel@icloud.com>
2024-04-20 22:12:54 +0200

    update v0.1.0

---
+++ dist/js/copper.js
@@ -0,0 +1,24 @@
+const onerror = false
+
+window.onerror = function (message, source, lineno, colno, error) {
+    if (!(message === "TypeError: i.createPopper is not a function") && onerror) {
+        var html = `
+            <div class="alert-w" role="alert">
+               <div><b>${message}</b> <br> <a target="_blank" href="${source}">${source}</a> Line: ${lineno} Column: ${colno}</div>
+            </div>`;
+        document.body.insertAdjacentHTML('beforeend', html);
+        document.title = `(! Error)`;
+    }
+};
+
+function p(value) {
+    console.log(value)
+}
+
+function display(value) {
+    var display = `
+    <div class="display" role="alert">
+        <div>${value}</div>
+    </div>`;
+    document.body.insertAdjacentHTML('beforeend', display);
+}
